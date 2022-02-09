d8ac1bfd232e91058e2484dc1c8a8de317096450
anztim <git@azti.me>
2022-02-10 04:27:43 +0800

    v0.5 坦克不再可以重叠

--- src/pers/aztim/tankwar/Player.java
+++ src/pers/aztim/tankwar/Player.java
@@ -4,4 +4,12 @@ public class Player extends Tank {
     public Player(int x, int y) {
         super(x, y,Identity.ALLY,5);
     }
+
+    @Override
+    public void step(Direction d) {
+        try {
+            super.step(d);
+        } catch (OutOfMap ignore) {
+        }
+    }
 }
--- src/pers/aztim/tankwar/Tank.java
+++ src/pers/aztim/tankwar/Tank.java
@@ -5,25 +5,80 @@ import java.util.Vector;
 public class Tank extends MovableElement {
     public static final int TANK_SIZE_X = 60;
     public static final int TANK_SIZE_Y = 60;
+    private static final TankMonitor TANK_MONITOR = TankMonitor.getTankMonitor();
+
+    private static class TankMonitor implements Runnable {
+        private static final TankMonitor tankMonitor = new TankMonitor();
+        private final Vector<Tank> allTanks;
+
+        private TankMonitor() {
+            allTanks = new Vector<>();
+            new Thread(this).start();
+        }
+
+        public void add(Tank tank) {
+            allTanks.add(tank);
+        }
+
+        public static TankMonitor getTankMonitor() {
+            return tankMonitor;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                // 两两检查是否碰撞，如果碰撞就恢复到上一步的状态
+                int len = allTanks.size();
+                Tank t1;
+                Tank t2;
+                for (int i = 0; i < len; ++i) {
+                    t1 = allTanks.get(i);
+                    for (int j = i + 1; j < len; ++j) {
+                        t2 = allTanks.get(j);
+                        if (MovableElement.isTouched(t1, t2)) {
+                            t1.recover();
+                            t2.recover();
+                        }
+                    }
+                }
+                // 清理已经消亡的坦克
+                allTanks.removeIf(tank -> !tank.isAlive());
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
     private final Identity identity;
     private final int bulletCapacity;
     private final Vector<Bullet> bullets;
     private int bulletSpeed = 15;
+    private int prevX;
+    private int prevY;
 
     public Tank(int x, int y, Identity identity, int bulletCapacity) {
         super(x, y, TANK_SIZE_X, TANK_SIZE_Y, Direction.UP, 3);
         this.identity = identity;
         this.bulletCapacity = bulletCapacity;
         bullets = new Vector<>();
+        this.prevX = x;
+        this.prevY = y;
+        TANK_MONITOR.add(this);
     }
 
     @Override
-    public void step(Direction d) {
-        try {
-            super.step(d);
-        } catch (OutOfMap ignore) {
-        }
+    public void stepForward() throws OutOfMap {
+        this.prevX = getX();
+        this.prevY = getY();
+        super.stepForward();
+    }
+
+    private void recover() {
+        this.setX(prevX);
+        this.setY(prevY);
     }
 
     public Bullet fire() {
