997b43d9c38aa66c4fab048bdefc4e1d0e2ecc3d
anztim <git@azti.me>
2022-02-10 06:00:18 +0800

    增加 allBullets 集合

--- src/pers/aztim/tankwar/Bullet.java
+++ src/pers/aztim/tankwar/Bullet.java
@@ -1,22 +1,32 @@
 package pers.aztim.tankwar;
 
+import java.util.Vector;
+
 public class Bullet extends MovableElement implements Runnable {
     public static final int BULLET_SIZE_X = 4;
     public static final int BULLET_SIZE_Y = 4;
 
+    private static final Vector<Bullet> allBullets = new Vector<>();
+
+    public static Vector<Bullet> getAllBullets() {
+        return allBullets;
+    }
+
     private final Identity identity;
     private final Tank host;
 
-    public Bullet(int x, int y, Direction direction, int speed,Identity identity, Tank host) {
+    public Bullet(int x, int y, Direction direction, int speed, Identity identity, Tank host) {
         super(x, y, BULLET_SIZE_X, BULLET_SIZE_Y, direction, speed);
         this.identity = identity;
         this.host = host;
+        allBullets.add(this);
     }
 
     @Override
     public void kill() {
         super.kill();
-        if(host != null && host.isAlive()) host.removeBullet(this);
+        if (host != null && host.isAlive()) host.removeBullet(this);
+        allBullets.remove(this);
     }
 
     @Override
